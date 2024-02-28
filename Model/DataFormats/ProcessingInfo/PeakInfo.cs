08ff3e2a7909371efa0df2c33c1c0610cb32acfe
HOME-PC\Nikita <nikitakononchik@gmail.com>
2024-02-28 16:34:43 +0300

    Info about calculated peaks are records

---
+++ Model/DataFormats/ProcessingInfo/PeakInfo.cs
@@ -0,0 +1,5 @@
+﻿namespace Model.DataFormats;
+public record PeakInfo(Spectra Spectra, float XStart, float XEnd, float Square, float Heigth) {
+    public override string ToString()
+        => $"{Spectra.Name}\t{Square: #.###}\t{Heigth: #.###}\t{XStart: #.###}\t{XEnd: #.###}";
+}
