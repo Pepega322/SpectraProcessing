c63ff86dc94eb01954553996ad7696445b97cc08
HOME-PC\Nikita <nikitakononchik@gmail.com>
2024-05-18 18:13:42 +0300

    Add dispersion statistics calculation for processed peaks

---
+++ MathStatistics/SpectraProcessing/SpectraProcessingDispersionStatistics.cs
@@ -0,0 +1,35 @@
+﻿using Domain.InputOutput;
+
+namespace MathStatistics.SpectraProcessing;
+
+public class SpectraProcessingDispersionStatistics : IWriteableData
+{
+	private readonly SpectraSetPeaks peaks;
+
+	public string Name { get; set; } = string.Empty;
+
+	internal SpectraProcessingDispersionStatistics(SpectraSetPeaks peaks)
+	{
+		this.peaks = peaks;
+	}
+
+	public IEnumerable<string> ToContents()
+	{
+		const string statisticsInfoHeader = "Parameter; xStart; xEnd; ValuesCount; AverageValue; StandardDeviation; ConfidenceInterval;";
+
+		yield return statisticsInfoHeader;
+		foreach (var borderSet in peaks.PeaksSets)
+		{
+			var borders = borderSet.First().Borders;
+			var square = borderSet.Select(p => p.Square).ToArray().GetDispersionStatistics();
+			yield return StatisticsFormat("Square", borders, square);
+			var height = borderSet.Select(p => p.Height).ToArray().GetDispersionStatistics();
+			yield return StatisticsFormat("Height", borders, height);
+		}
+
+		yield break;
+
+		string StatisticsFormat(string parameter, PeakBorders borders, DispersionStatistics<float> s)
+			=> $"{parameter}; {borders.XStart}; {borders.XEnd}; {s.ValuesCount}; {s.AverageValue: 0.000}; {s.StandardDeviation: 0.000}; {s.ConfidenceInterval: 0.000};";
+	}
+}
--- MathStatistics/SpectraProcessing/SpectrasProcessingResult.cs
+++ MathStatistics/SpectraProcessing/SpectrasProcessingResult.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using Domain.InputOutput;
+﻿using Domain.InputOutput;
 
 namespace MathStatistics.SpectraProcessing;
 
@@ -14,10 +13,21 @@ public class SpectrasProcessingResult : IWriteableData
 		peaks.Add(peak);
 	}
 
+	public SpectraProcessingDispersionStatistics GetMetrology() => new SpectraProcessingDispersionStatistics(peaks);
+
 	public IEnumerable<string> ToContents()
 	{
-		yield return "XStart XEnd Spectra Square Height";
-		
-		//TODO Add metrology calculations
+		const string peakInfoHeader = "XStart; XEnd; Spectra; Square; Height;";
+
+		yield return peakInfoHeader;
+		foreach (var borderSet in peaks.PeaksSets)
+		{
+			foreach (var peak in borderSet.OrderByDescending(p => p.SpectraName))
+				yield return PeakFormat(peak);
+		}
+
+		yield break;
+
+		string PeakFormat(SpectraPeak p) => $"{p.Borders.XStart: 0.000}; {p.Borders.XEnd: 0.000}; {p.SpectraName}; {p.Square: 0.000}; {p.Height: 0.000};";
 	}
 }
