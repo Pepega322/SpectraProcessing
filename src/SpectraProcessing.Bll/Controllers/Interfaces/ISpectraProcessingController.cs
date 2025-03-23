3ea5e9f73f72a1e62bd61b6d429d139c92ad61db
HOME-PC\Nikita <nikitakononchik@gmail.com>
2025-03-23 03:09:01 +0300

    updated SpectraProcessingController

---
+++ src/SpectraProcessing.Bll/Controllers/Interfaces/ISpectraProcessingController.cs
@@ -0,0 +1,15 @@
+﻿using SpectraProcessing.Models.Peak;
+using SpectraProcessing.Models.Spectra.Abstractions;
+
+namespace SpectraProcessing.Bll.Controllers.Interfaces;
+
+public interface ISpectraProcessingController
+{
+    event Action? OnPlotAreaChanged;
+
+    Task AddPeak(PeakData peak);
+
+    Task RemovePeak(PeakData peak);
+
+    Task SaveSpectraPeaks(SpectraData spectra);
+}
---
+++ src/SpectraProcessing.Bll/Controllers/SpectraProcessingController.cs
@@ -0,0 +1,27 @@
+﻿using SpectraProcessing.Bll.Controllers.Interfaces;
+using SpectraProcessing.Models.Collections.Keys;
+using SpectraProcessing.Models.Peak;
+using SpectraProcessing.Models.Spectra.Abstractions;
+
+namespace SpectraProcessing.Bll.Controllers;
+
+public class SpectraProcessingController(IDataStorageProvider<SpectraKey, PeakDataPlot> peaksStorage)
+    : ISpectraProcessingController
+{
+    public event Action? OnPlotAreaChanged;
+
+    public Task AddPeak(PeakData peak)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemovePeak(PeakData peak)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveSpectraPeaks(SpectraData spectra)
+    {
+        throw new NotImplementedException();
+    }
+}
