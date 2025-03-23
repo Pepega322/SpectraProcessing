258fc850b0665bf99350c6079ff055cc9e6fad7e
HOME-PC\Nikita <nikitakononchik@gmail.com>
2025-03-23 03:06:18 +0300

    added peak controller

---
+++ src/SpectraProcessing.Bll/Controllers/Interfaces/IPeakController.cs
@@ -0,0 +1,12 @@
+﻿using SpectraProcessing.Models.Peak;
+
+namespace SpectraProcessing.Bll.Controllers.Interfaces;
+
+public interface IPeakController
+{
+    event Action? OnPeakChanges;
+
+    Task<PeakDataPlot?> TryGetPeak();
+
+    Task TryMovePeak();
+}
