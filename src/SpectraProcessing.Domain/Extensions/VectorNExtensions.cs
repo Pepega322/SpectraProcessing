60e98de17a6e1cb4e14d83b53a332db1ca970614
HOME-PC\Nikita <nikitakononchik@gmail.com>
2025-03-26 02:10:48 +0300

    added VectorN

---
+++ src/SpectraProcessing.Domain/Extensions/VectorNExtensions.cs
@@ -0,0 +1,51 @@
+﻿using SpectraProcessing.Domain.Models.MathModels;
+
+namespace SpectraProcessing.Domain.Extensions;
+
+public static class VectorNExtensions
+{
+    public static VectorN Sum(this IReadOnlyCollection<VectorN> vectors)
+    {
+        var dimension = vectors.Select(x => x.Dimension).Distinct().Single();
+
+        var values = new double[dimension];
+
+        for (var d = 0; d < dimension; d++)
+        {
+            values[d] = vectors.Sum(v => v[d]);
+        }
+
+        return new VectorN(values);
+    }
+
+    public static VectorN Scalar(this VectorN left, VectorN right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new ArgumentException("Dimensions don't match");
+        }
+
+        var values = new double[left.Dimension];
+
+        for (var d = 0; d < left.Dimension; d++)
+        {
+            values[d] = left[d] * right[d];
+        }
+
+        return new VectorN(values);
+    }
+
+    public static VectorN Normalize(this VectorN vector)
+    {
+        var length = vector.Length();
+
+        var values = vector.Values.Select(v => v / length).ToArray();
+
+        return new VectorN(values);
+    }
+
+    public static double Length(this VectorN vector)
+    {
+        return Math.Sqrt(vector.Values.Sum(x => x * x));
+    }
+}
---
+++ src/SpectraProcessing.Domain/Models/MathModels/VectorN.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+
+namespace SpectraProcessing.Domain.Models.MathModels;
+
+public sealed class VectorN
+{
+    private readonly double[] values;
+
+    public IReadOnlyList<double> Values => values;
+
+    public int Dimension => values.Length;
+
+    public double this[int index] => values[index];
+
+    public VectorN(IReadOnlyList<double> values)
+    {
+        this.values = values.ToArray();
+    }
+
+    public override bool Equals(object? obj) => obj is VectorN vectorN && vectorN == this;
+
+    public override int GetHashCode() => values.GetHashCode();
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+
+        builder.Append($"Dimension: {Dimension}, ");
+        builder.Append("Values: {");
+
+        for (var d = 0; d < Dimension; d++)
+        {
+            builder.Append(values[d].ToString("0.##"));
+            if (d < Dimension - 1)
+            {
+                builder.Append(", ");
+            }
+        }
+
+        builder.Append("}");
+
+        return builder.ToString();
+    }
+
+#region Operators
+
+    public static VectorN operator +(VectorN left, VectorN right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new ArgumentException("Dimensions don't match");
+        }
+
+        var values = new double[left.Dimension];
+
+        for (var d = 0; d < left.Dimension; d++)
+        {
+            values[d] = left.Values[d] + right.Values[d];
+        }
+
+        return new VectorN(values);
+    }
+
+    public static VectorN operator -(VectorN left, VectorN right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new ArgumentException("Dimensions don't match");
+        }
+
+        var values = new double[left.Dimension];
+
+        for (var d = 0; d < left.Dimension; d++)
+        {
+            values[d] = left.Values[d] - right.Values[d];
+        }
+
+        return new VectorN(values);
+    }
+
+    public static VectorN operator *(VectorN v, double multiplier)
+    {
+        var values = new double[v.Dimension];
+
+        for (var d = 0; d < v.Dimension; d++)
+        {
+            values[d] = v.Values[d] * multiplier;
+        }
+
+        return new VectorN(values);
+    }
+
+    public static VectorN operator *(double multiplier, VectorN v)
+    {
+        return v * multiplier;
+    }
+
+    public static VectorN operator /(VectorN v, double divider)
+    {
+        return v * (1 / divider);
+    }
+
+    public static bool operator ==(VectorN left, VectorN right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new ArgumentException("Dimensions don't match");
+        }
+
+        for (var d = 0; d < left.Dimension; d++)
+        {
+            if (Math.Abs(left.Values[d] - right.Values[d]) > 1e-6)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool operator !=(VectorN left, VectorN right)
+    {
+        return !(left == right);
+    }
+
+#endregion
+}
