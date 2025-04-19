fcb777bdf6092e329c1c1c81f679c5a8f5428922
NikitaKononchik <nkononchik@ozon.ru>
2025-04-19 16:29:15 +0300

    t

---
+++ tests/SpectraProcessing.Tests/Tests/AirPLSBaselineCorrectionTests.cs
@@ -0,0 +1,13 @@
+using SpectraProcessing.Domain.Models.MathModeling.Common;
+using SpectraProcessing.TestingInfrastructure;
+using Xunit;
+
+namespace SpectraProcessing.Tests.Tests;
+
+public class AirPLSBaselineCorrectionTests
+{
+    [Fact]
+    public async Task CorrectBaseline_Success()
+    {
+    }
+}
