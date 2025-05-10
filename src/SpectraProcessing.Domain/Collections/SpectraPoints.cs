f32df53705115137078ebf205429cbf78ba69268
HOME-PC\Nikita <nikitakononchik@gmail.com>
2025-05-10 16:23:54 +0300

    [refactoring] changed smoothing

--- src/SpectraProcessing.Domain/Collections/SpectraPoints.cs
+++ src/SpectraProcessing.Domain/Collections/SpectraPoints.cs
@@ -23,30 +23,14 @@ public sealed record SpectraPoints
 
     public void Smooth()
     {
-        const int centerIndex = 2;
         Span<float> tempY = stackalloc float[Count];
-        Span<float> nearPoints = stackalloc float[7];
 
-        for (var i = 0; i < Count; i++)
+        tempY[0] = (Y[0] + 2 * Y[1]) / 3;
+        tempY[Count - 1] = (Y[Count - 1] + 2 * Y[Count - 2]) / 3;
+
+        for (var i = 1; i < Count - 1; i++)
         {
-            nearPoints[centerIndex] = Y[i];
-
-            for (var j = 1; j < 3; j++)
-            {
-                var leftIndex = i - j < 0
-                    ? i + j
-                    : i - j;
-
-                var rightIndex = i + j >= Count
-                    ? i - j
-                    : i + j;
-
-                nearPoints[centerIndex - j] = Y[leftIndex];
-                nearPoints[centerIndex + j] = Y[rightIndex];
-            }
-
-            tempY[i] = (nearPoints[0] + 3 * nearPoints[1] + 6 * nearPoints[2] + 7 * nearPoints[3] + 6 * nearPoints[4] +
-                3 * nearPoints[5] + nearPoints[6]) / 27;
+            tempY[i] = (Y[i - 1] + Y[i] + Y[i + 1]) / 3;
         }
 
         for (var i = 0; i < Count; i++)
@@ -54,6 +38,39 @@ public sealed record SpectraPoints
             Y[i] = tempY[i];
         }
     }
+    // public void Smooth()
+    // {
+    //     const int centerIndex = 2;
+    //     Span<float> tempY = stackalloc float[Count];
+    //     Span<float> nearPoints = stackalloc float[7];
+    //
+    //     for (var i = 0; i < Count; i++)
+    //     {
+    //         nearPoints[centerIndex] = Y[i];
+    //
+    //         for (var j = 1; j < 3; j++)
+    //         {
+    //             var leftIndex = i - j < 0
+    //                 ? i + j
+    //                 : i - j;
+    //
+    //             var rightIndex = i + j >= Count
+    //                 ? i - j
+    //                 : i + j;
+    //
+    //             nearPoints[centerIndex - j] = Y[leftIndex];
+    //             nearPoints[centerIndex + j] = Y[rightIndex];
+    //         }
+    //
+    //         tempY[i] = (nearPoints[0] + 3 * nearPoints[1] + 6 * nearPoints[2] + 7 * nearPoints[3] + 6 * nearPoints[4] +
+    //             3 * nearPoints[5] + nearPoints[6]) / 27;
+    //     }
+    //
+    //     for (var i = 0; i < Count; i++)
+    //     {
+    //         Y[i] = tempY[i];
+    //     }
+    // }
 
     public void Transform(Func<float, float, float> transformRule)
     {
